da8c55077a0b9973fbbc002189eb1086e1d431f6
Rulfy <git@zynox.net>
2015-12-16 15:06:48 +0100

    hiding cookie info from the top

--- az-paywall-block.js
+++ az-paywall-block.js
@@ -4,18 +4,23 @@
 // @description Clearing your cookies so that you won't see the paywall.
 // @include     http://augsburger-allgemeine.de/*
 // @include     http://www.augsburger-allgemeine.de/*
-// @version     1
+// @require     http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js
+// @version     2
 // @grant       none
 // @run-at      document-end
 // ==/UserScript==
 function createCookie(name) {
-    var date = new Date();
-    date.setTime(date.getTime()+(-1*24*60*60*1000));
-    var expires = "; expires="+date.toGMTString();
-    document.cookie = name+"="+expires+"; path=/";
+	var date = new Date();
+	date.setTime(date.getTime() + ( -1 * 24 * 60 * 60 * 1000));
+	var expires = '; expires=' + date.toGMTString();
+	document.cookie = name + '=' + expires + '; path=/';
 }
 
-
-var cookies = document.cookie.split(";");
+var cookies = document.cookie.split(';');
 for (var i = 0; i < cookies.length; i++)
-      createCookie(cookies[i]);
+	createCookie(cookies[i]);
+
+// remove cookie info
+var info = $('#cookie_info');
+if( info.length > 0 )
+    info.remove();
